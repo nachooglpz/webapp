4c23b8c28ac0001714078728503147ea5e8a0058
Luis Ignacio <87046698+nachooglpz@users.noreply.github.com>
2024-11-06 13:25:30 +0000

    Creo que no funciona

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:test@localhost:3306/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(100), nullable = False)
+    last_name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(80), unique = True, nullable = False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone = True), server_default = func.now())
+    bio = db.Column(db.Text)
+
+def __repr__(self):
+        return f'<Student {self.first_name}>'
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world!</p>"
+
+@app.route("/usuario/<name>")
+def hello_usuario(name):
+    return f"<p>Hello, {name}!</p>"
